4a8f56bcec3c4fc182bbabe20efa31900b5b30dc
priyonrato.das <priyodas12@gmail.com>
2024-07-30 11:33:00 +0530

    initial commit

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ProductComponent } from '../product/product.component';
+
+@Component({
+  selector: 'app-product-list',
+  standalone: true,
+  imports: [ProductComponent],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent {
+
+}
